d4c46fa06af22badda4807699d72851b21c1b8a3
mohan <mohan@123.com>
2023-03-11 18:43:34 +0800

    Changes

--- mohan-blog/src/main/java/com/mohan/config/SecurityConfig.java
+++ mohan-blog/src/main/java/com/mohan/config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/logout").authenticated()
             .antMatchers("/user/userInfo").authenticated()
 //            .antMatchers("/upload").authenticated()
+            .antMatchers("/comment").authenticated()
             // 除上面外的所有请求全部不需要认证即可访问
             .anyRequest().permitAll();
 
