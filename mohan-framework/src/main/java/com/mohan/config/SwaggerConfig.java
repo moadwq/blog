025c9ae1573b1a5b9e453237b45922f97c19c6f1
mohan <mohan@123.com>
2023-03-11 19:02:20 +0800

    Changes

---
+++ mohan-framework/src/main/java/com/mohan/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.mohan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * 配置接口文档信息
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket customDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mohan.controller"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("mohan", "http://www.mohan.com", "1485804541@qq.com");
+        return new ApiInfoBuilder()
+                .title("前台接口文档")
+                .description("123")
+                .contact(contact)   // 联系方式
+                .version("1.1.0")  // 版本
+                .build();
+    }
+}
